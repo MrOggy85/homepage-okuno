f2ee0def219b1c1116a56903f477e15830c7ece5
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-06-15 07:08:40 +0900

    init

---
+++ main.ts
@@ -0,0 +1,35 @@
+import { serve } from "./deps.ts";
+
+const PORT = Number(Deno.env.get("PORT")) || 3000;
+
+const STATIC_FILES: Record<string, string> = {
+  '/static/reset.css': "text/css",
+  '/static/main.css': "text/css",
+  '/static/favicon.ico': "image/x-icon",
+};
+
+async function handler(req: Request): Promise<Response> {
+  const { pathname } = new URL(req.url);
+  console.log('pathname', pathname);
+  if (pathname.startsWith("/static")) {
+    const contentType = STATIC_FILES[pathname];
+    console.log('contentType', contentType);
+
+    const file = await Deno.readFile(`${Deno.cwd()}/${pathname}`);
+    return new Response(file, {
+      headers: {
+        "content-type": contentType,
+      },
+    });
+  }
+
+  const file = await Deno.readFile("./index.html");
+  return new Response(file, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
+
+console.log(`Server is listening on port ${PORT}`);
+await serve(handler, { port: PORT });
